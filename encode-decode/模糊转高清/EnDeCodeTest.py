e8a818f44bae71fbe6cc97eba52951ff0986bba5
zhuxiaodong <zhuxiaodong@eenet.com>
2024-09-05 16:32:49 +0800

    生成压缩文件

--- encode-decode/模糊转高清/EnDeCodeTest.py
+++ encode-decode/模糊转高清/EnDeCodeTest.py
@@ -3,40 +3,101 @@
 '''
 import os
 
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+import numpy as np
+from PIL import Image
+from matplotlib import transforms
 from tensorflow.python.keras import Input
-from tensorflow.python.keras.layers import Conv2D, ZeroPadding2D, LeakyReLU, BatchNormalization, UpSampling2D, \
+from tensorflow.python.keras.layers import Conv2D, LeakyReLU, BatchNormalization, UpSampling2D, \
     MaxPooling2D
-from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.losses import BCE
-import tensorflow as tf
-import tensorflow.python.keras.layers
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.types.core import Tensor
 
 IMAGE_ORDERING = 'channels_last'
 
-test_image = ""
-
-
-def format_image(image_path):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [224, 224])
-    return image
+# A_path = "D:/work/pic/测试图片/110101199603010001.jpg"
+# A_path = "D:/work/pic/20240716152854.png"
+test_image = "D:/work/pic/1721203779179.png"
 
 
 # 图片转数据
-def image_to_data():
-    pass
+def image_to_data(image):
+    return transforms.Compose([transforms.Resize((224, 224)),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])(image)
 
 
-# 数据转图片
-def data_to_image():
-    pass
-
-
+# 保存图片
 def save_image(epoch, prediction, path="image"):
     if not os.path.exists("./images"):
         os.makedirs("./images")
+    file_name = path + "/" + epoch + ".png"
+    save_image(tensor2im(prediction), file_name)
+
+
+def save_image(image_numpy, image_path, aspect_ratio=1.0):
+    """Save a numpy image to the disk
+
+    Parameters:
+        image_numpy (numpy array) -- input numpy array
+        image_path (str)          -- the path of the image
+        aspect_ratio:
+    """
+    image_pil = Image.fromarray(image_numpy)
+    h, w, _ = image_numpy.shape
+
+    if aspect_ratio > 1.0:
+        image_pil = image_pil.resize((h, int(w * aspect_ratio)), Image.BICUBIC)
+    if aspect_ratio < 1.0:
+        image_pil = image_pil.resize((int(h / aspect_ratio), w), Image.BICUBIC)
+    image_pil.save(image_path)
+
+
+# 数据转图片
+def tensor2im(input_image, im_type=np.uint8):
+    """"Converts a Tensor array into a numpy image array.
+
+    Parameters:
+        input_image (tensor) --  the input image tensor array
+        im_type (type)        --  the desired type of the converted numpy array
+    """
+    if not isinstance(input_image, np.ndarray):
+        if isinstance(input_image, Tensor):  # get the data from a variable
+            image_tensor = input_image.data
+        else:
+            return input_image
+        image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
+        if image_numpy.shape[0] == 1:  # grayscale to RGB
+            image_numpy = np.tile(image_numpy, (3, 1, 1))
+        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
+    else:  # if it is a numpy array, do nothing
+        image_numpy = input_image
+    return image_numpy.astype(im_type)
+
+
+# 创建基础图片
+def create_pic(path, n_path):
+    path_list = [path + "/" + pic for pic in os.listdir(path)]
+    for file in path_list:
+        im = Image.open(file)
+        # 原始大小
+        (x, y) = im.size
+        # 标准，比例
+        n_x = 224
+        n_y = int(y * n_x / x)
+
+        # 改变尺寸，保持图片高品质
+        out = im.resize((n_x, n_y), Image.ANTIALIAS)
+
+        if out.mode == 'RGBA':
+            out = out.convert('RGB')
+
+        if not os.path.exists(n_path):
+            os.makedirs(n_path)
+        out.save(n_path + "/{}".format(file.split("/")[-1]))
 
 
 class VagueToHD:
@@ -166,4 +227,4 @@ class VagueToHD:
 
 
 if __name__ == '__main__':
-    pass
+    create_pic("E:/train/gan/DIV2K_train_HR/DIV2K_train_HR", "E:/train/gan/DIV2K_train_HR/DIV2K_train")
